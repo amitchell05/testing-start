d442431247a920361bb8eb880334c6a1af2a0bdc
Angela Mitchell <anglarenmit@gmail.com>
2021-01-25 12:52:50 -0600

    Replace toBeTruthy() with toEqual() for reversePipe test

--- src/app/shared/reverse.pipe.spec.ts
+++ src/app/shared/reverse.pipe.spec.ts
@@ -3,6 +3,6 @@ import { ReversePipe } from './reverse.pipe';
 describe('Pipe: ReversePipe', () => {
   it('should reverse a string', () => {
     let reversePipe = new ReversePipe();
-    expect(reversePipe.transform('hello')).toBeTruthy('olleh');
+    expect(reversePipe.transform('hello')).toEqual('olleh');
   });
 });
